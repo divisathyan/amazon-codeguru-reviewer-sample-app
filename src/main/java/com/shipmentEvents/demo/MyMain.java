23b3fa8ff6baad2befd53c38fb0c89a0033c978f
Divya Arun <divya.arun@enginegroup.com>
2021-06-20 20:33:53 +0100

    jar renamed

--- src/main/java/com/shipmentEvents/demo/MyMain.java
+++ src/main/java/com/shipmentEvents/demo/MyMain.java
@@ -1,12 +1,14 @@
 package com.shipmentEvents.demo;
 
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import software.amazon.codeguruprofilerjavaagent.Profiler;
 
 public class MyMain {
     public static void main(String[] args) {
-        Profiler.builder()
-            .profilingGroupName("MyProfilingGroup")
-            .build()
-            .start();
+        EnvironmentVariableCredentialsProvider authProvider = new EnvironmentVariableCredentialsProvider();
+        new Profiler.Builder()
+            .profilingGroupName("divya-sample-app")
+            .withHeapSummary(true) // optional - to start without heap profiling set to false or remove line
+            .build().start();
     }
 }
